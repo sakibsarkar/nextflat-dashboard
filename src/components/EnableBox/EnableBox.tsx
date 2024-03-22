d13e291c453756220b29b2b34b1733b0c5eab842
sakibsarkar <sakibsarkar707@gmail.com>
2024-03-22 22:39:26 +0600

    active inactive toggle button created

--- src/components/EnableBox/EnableBox.tsx
+++ src/components/EnableBox/EnableBox.tsx
@@ -1,13 +1,21 @@
+"use client";
+import DisableIcon from "@/utils/icons/DisableIcon";
 import EnableIcon from "@/utils/icons/EnableIcon";
-import React from "react";
-
-const EnableBox = ({text}: {text: string}) => {
-    return (
-        <div className="flex items-center gap-3">
-            <EnableIcon />
-            <div>{text}</div>
-        </div>
-    );
+import React, { useState } from "react";
+const EnableBox: React.FC<{ text: string; defaultValue?: Boolean }> = ({
+  text,
+  defaultValue,
+}) => {
+  const [state, setState] = useState<Boolean>(defaultValue || true);
+  return (
+    <div
+      className="flex items-center gap-3 cursor-pointer"
+      onClick={() => setState(!state)}
+    >
+      <div className="w-[67px]">{state ? <EnableIcon /> : <DisableIcon />}</div>
+      <div>{text}</div>
+    </div>
+  );
 };
 
 export default EnableBox;
